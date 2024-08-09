336b138a89eca6f6f2e7deafb42d9063900d3cbe
Beverly1310 <155519926+Beverly1310@users.noreply.github.com>
2024-08-09 09:05:40 +0700

    Revert "login fe"

--- public/src/constants/status.js
+++
@@ -1,4 +0,0 @@
-export const PENDING = "pending";
-export const SUCCESS = "success";
-export const FAILED = "failed";
-export const IDLE = "idle";
--- public/src/pages/Company.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Company() {
-  return (
-    <div>
-    <h1>company</h1>
-    </div>
-  )
-}
